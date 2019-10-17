3f21c50aaef7f542efef92af7149bbe750a06bdc
MRDEO24 <muhammadraihanarrasyid24@gmail.com>
2019-10-17 14:20:45 +0700

    Update

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
-('.page-scroll').on('click', function(){
-	
-	var href =$(this).attr('href');
-	
-	
-	
-})
+//fungsi untuk creation
+function Fungsi(imgs) {
+  // Get the expanded image
+  var expandImg = document.getElementById("expandedImg");
+  // Get the image text
+  var imgText = document.getElementById("imgtext");
+  // Use the same src in the expanded image as the image being clicked on from the grid
+  expandImg.src = imgs.src;
+  // Use the value of the alt attribute of the clickable image as text inside the expanded image
+  imgText.innerHTML = imgs.alt;
+  // Show the container element (hidden with CSS)
+  expandImg.parentElement.style.display = "block";
+}
